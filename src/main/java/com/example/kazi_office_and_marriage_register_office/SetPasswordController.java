1872ec67125368cb1481dcfd7f36a10633ce82f0
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-26 14:02:28 +0600

    Add Set Password - Field: Verify code, new password, confirm password -button: back to login, reset password

---
+++ src/main/java/com/example/kazi_office_and_marriage_register_office/SetPasswordController.java
@@ -0,0 +1,13 @@
+package com.example.kazi_office_and_marriage_register_office;
+
+import javafx.event.ActionEvent;
+
+public class SetPasswordController {
+    @javafx.fxml.FXML
+    public void resetPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backToLoginPageOnAction(ActionEvent actionEvent) {
+    }
+}
