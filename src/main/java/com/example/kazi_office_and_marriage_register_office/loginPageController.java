42e80f712d6683b1d2c1b0fb5148e041bb8d6a45
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-26 13:08:45 +0600

    Added Login page.
    -login page with username,password,role,forget password and create new account

---
+++ src/main/java/com/example/kazi_office_and_marriage_register_office/loginPageController.java
@@ -0,0 +1,17 @@
+package com.example.kazi_office_and_marriage_register_office;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class loginPageController {
+    @javafx.fxml.FXML
+    private AnchorPane loginViewPage;
+
+    @javafx.fxml.FXML
+    public void loginButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createNewButtonOnAction(ActionEvent actionEvent) {
+    }
+}
