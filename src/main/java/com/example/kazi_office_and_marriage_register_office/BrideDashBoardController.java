140fb12fe1853ac407ba1b4cbfe36ecda122b3da
yasinhossain2312080 <2312080@iub.edu.bd>
2026-07-28 02:38:19 +0600

    Complete Goal 2 (switching) complete Goal 3 (switching)

--- src/main/java/com/example/kazi_office_and_marriage_register_office/BrideDashBoardController.java
+++ src/main/java/com/example/kazi_office_and_marriage_register_office/BrideDashBoardController.java
@@ -2,8 +2,10 @@ package com.example.kazi_office_and_marriage_register_office;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -39,7 +41,9 @@ public class BrideDashBoardController
     }
 
     @javafx.fxml.FXML
-    public void scheduleMarriageDateButtonOnAction(ActionEvent actionEvent) {
+    public void scheduleMarriageDateButtonOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("schedule-marriage-view.fxml"));
+        boarderPaneDashBoard.setCenter(fxmlLoader.load());
     }
 
     @javafx.fxml.FXML
@@ -51,6 +55,12 @@ public class BrideDashBoardController
     }
 
     @javafx.fxml.FXML
-    public void uploadDocumentsButtonOnAction(ActionEvent actionEvent) {
+    public void uploadDocumentsButtonOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("upload-documents-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage nextStage =(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        nextStage.setTitle("Upload Documents!");
+        nextStage.setScene(scene);
+        nextStage.show();
     }
 }
