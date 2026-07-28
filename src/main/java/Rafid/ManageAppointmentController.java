92ebc55523f3f48f47039dab33457ec3cd359a01
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-28 13:38:00 +0600

    Add Manage Appointment - Feature: Bride/Groom info, Appointment details, time and Date, Filter - buttons: Confirm, Cancel, Search

---
+++ src/main/java/Rafid/ManageAppointmentController.java
@@ -0,0 +1,21 @@
+package Rafid;
+
+import javafx.event.ActionEvent;
+
+public class ManageAppointmentController {
+    @javafx.fxml.FXML
+    public void cancelButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FilterSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void conFirmButtonOnAction(ActionEvent actionEvent) {
+    }
+}
