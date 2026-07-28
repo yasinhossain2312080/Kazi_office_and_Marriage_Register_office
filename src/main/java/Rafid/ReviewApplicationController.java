e52fe1fc6e66a9f104f28f6866dd5369139fcb36
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-28 12:47:08 +0600

    Add review application - Feature: Bride/Groom info, application details, documents - buttons: Verify Bride/Groom, Approve/Reject Marriage

---
+++ src/main/java/Rafid/ReviewApplicationController.java
@@ -0,0 +1,25 @@
+package Rafid;
+
+import javafx.event.ActionEvent;
+
+public class ReviewApplicationController {
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveMarriageOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyBrideOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyGroomOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectMarriageOnAction(ActionEvent actionEvent) {
+    }
+}
