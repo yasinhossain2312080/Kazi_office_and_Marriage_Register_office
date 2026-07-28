10fa2b3e27f47bbad9ab559f00b9c2e5f8224847
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-28 18:37:58 +0600

    Add Generate Report - Feature: Generate Report, Clear, Download

---
+++ src/main/java/Rafid/GenerateReportController.java
@@ -0,0 +1,21 @@
+package Rafid;
+
+import javafx.event.ActionEvent;
+
+public class GenerateReportController {
+    @javafx.fxml.FXML
+    public void generateReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadReeportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearGenerateReportOnAction(ActionEvent actionEvent) {
+    }
+}
