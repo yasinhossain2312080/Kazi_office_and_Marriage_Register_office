7254b08216ccd395f0ddb624663bb42f7f6a5c7a
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-28 18:10:14 +0600

    Add Certificate Management Feature: Search, Issue Duplicate, Verify Certificate

---
+++ src/main/java/Rafid/CertificateManagementController.java
@@ -0,0 +1,21 @@
+package Rafid;
+
+import javafx.event.ActionEvent;
+
+public class CertificateManagementController {
+    @javafx.fxml.FXML
+    public void issueDuplicateOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyCertificateOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchRegisteredMarriageFilterOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/Rafid/RegistryManagementController.java
+++ src/main/java/Rafid/RegistryManagementController.java
@@ -3,19 +3,21 @@ package Rafid;
 import javafx.event.ActionEvent;
 
 public class RegistryManagementController {
-    @javafx.fxml.FXML
-    public void UpdateMarriageRegistryOnAction(ActionEvent actionEvent) {
-    }
 
     @javafx.fxml.FXML
     public void BackOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
-    public void archiveMarriageRegistryOnAction(ActionEvent actionEvent) {
-    }
 
     @javafx.fxml.FXML
     public void searchRegisteredMarriageFilterOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueDuplicateOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyCertificateOnAction(ActionEvent actionEvent) {
     }
 }
