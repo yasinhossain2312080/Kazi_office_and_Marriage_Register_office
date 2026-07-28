a238114e773552dad6998c0d86be5bc463b5e832
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-28 17:21:19 +0600

    Add Register Marriage - Feature: Add approved marriage to registered list

---
+++ src/main/java/Rafid/RegisterMarriageController.java
@@ -0,0 +1,17 @@
+package Rafid;
+
+import javafx.event.ActionEvent;
+
+public class RegisterMarriageController {
+    @javafx.fxml.FXML
+    public void registerMarriageOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchApprovedMarriageFilterOnAction(ActionEvent actionEvent) {
+    }
+}
