b943af86c6e5db379e8cf991b5b382c7c918c1ee
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-28 14:21:57 +0600

    Add Search Marriage Record -Feature: Filter to search records, table view, see record

---
+++ src/main/java/Rafid/SearchMarriageRecordController.java
@@ -0,0 +1,17 @@
+package Rafid;
+
+import javafx.event.ActionEvent;
+
+public class SearchMarriageRecordController {
+    @javafx.fxml.FXML
+    public void searchMarriageRecordFilterOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearMarriageRecordFilterOnAction(ActionEvent actionEvent) {
+    }
+}
