4840ed4f7f9adab1a0831bbc08af2c4e4e0a3ea2
rafideSarker2521062 <rafidsharker@gmail.com>
2026-07-29 23:00:50 +0600

    add admin dashboard

--- src/main/java/com/example/kazi_office_and_marriage_register_office/sayed/adminDashboardController.java
+++ src/main/java/com/example/kazi_office_and_marriage_register_office/sayed/adminDashboardController.java
@@ -1,4 +1,45 @@
 package com.example.kazi_office_and_marriage_register_office.sayed;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.BorderPane;
+
 public class adminDashboardController {
+    @javafx.fxml.FXML
+    private BorderPane borderPane;
+
+    @javafx.fxml.FXML
+    public void generateDailyReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateMonthlyReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SearchPaymentRecordsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void collectRegistrationFeeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyPaymentTransactionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void processRefundRequestButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generatePaymentReceiptButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportFinancialReportButtonOnAction(ActionEvent actionEvent) {
+    }
 }
